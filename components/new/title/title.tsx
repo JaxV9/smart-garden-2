211992ed74a22e413b9ecf61515182a556614bd6
JaxV9 <layanj.pro@gmail.com>
2025-12-02 20:21:59 +0100

    feat: home navigation

---
+++ components/new/title/title.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text } from "react-native";
+
+interface TitleProps {
+    text: string
+}
+
+export function Title({ text }: TitleProps) {
+
+    return (
+        <Text style={styles.text}>{text}</Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 20,
+        fontWeight: 500
+    }
+});
