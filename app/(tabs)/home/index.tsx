d444a0c496e1c08e2790770198e048a1abe60e74
JaxV9 <layanj.pro@gmail.com>
2025-12-03 13:14:12 +0100

    feat: load garden vegetables and vegetables"

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -2,6 +2,8 @@ import { CalendarComp } from '@/components/new/calendar/calendar';
 import { HomeSection, NavBarGardenSection } from '@/components/new/navGardenSection/navbar';
 import { useVegetablesContext } from '@/contexts/vegetables.context';
 import { useCalendar } from '@/hooks/useCalendar';
+import { useGarden } from '@/hooks/useGarden';
+import { useVegetable } from '@/hooks/useVegetable';
 import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
@@ -10,11 +12,19 @@ export default function Index() {
 
     const [currentSection, setCurrentSection] = useState<HomeSection>('calendar')
     const { calendar } = useCalendar()
-    const { loadVegetables } = useVegetablesContext()
+    const { loadGardenVegetables } = useGarden()
+    const { vegetablesContext } = useVegetablesContext()
+    const { loadVegetables } = useVegetable()
 
     useEffect(() => {
         loadVegetables()
     }, [])
+
+    useEffect(() => {
+        if (vegetablesContext.length > 0) {
+            loadGardenVegetables()
+        }
+    }, [vegetablesContext])
 
     return (
         <View style={styles.container}>
