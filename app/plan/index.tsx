ef04e34d512a33db50411a3ad45a85be704b8710
failaxite <48534490+failaxite@users.noreply.github.com>
2026-05-23 11:52:29 +0200

    :bug: Fixed header positioning to allow interaction and navigation back

--- app/plan/index.tsx
+++ app/plan/index.tsx
@@ -5,14 +5,15 @@ import { StyleSheet, View } from "react-native";
 export default function Index() {
     return (
         <View style={styles.container}>
-            <AppHeader
-                title="Plan"
-                showBack={true}
-                showNotifications={true}
-                fallbackRoute="/home"
-            />
-
             <Plan />
+            <View style={styles.headerContainer}>
+                <AppHeader
+                    title="Plan"
+                    showBack={true}
+                    showNotifications={true}
+                    fallbackRoute="/home"
+                />
+            </View>
         </View>
     );
 }
@@ -21,5 +22,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#F9FAFB',
+    },
+    headerContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 100,
     },
 });
